3aeb6fb1e0020dd841abf312cb196907a8f4e34f
georgekaran <georgekaran12@gmail.com>
2020-09-04 16:49:10 -0300

    test: ensure Main component match snapshot

--- src/components/Main/test.spec.tsx
+++ src/components/Main/test.spec.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, RenderResult } from '@testing-library/react'
 
 import Main from './index'
 
+const makeSut = (): RenderResult => {
+  return render(<Main />)
+}
+
 describe('Main component', () => {
   test('Should render with no error', () => {
-    render(<Main />)
+    makeSut()
     expect(screen.getByTestId('title')).toHaveTextContent('Won-Games')
+  })
+
+  test('Should match snapshot', () => {
+    const { container } = makeSut()
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
