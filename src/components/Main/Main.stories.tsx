888dcef87725173c24e4e1b975ebf906149a0455
georgekaran <georgekaran12@gmail.com>
2020-09-06 18:31:27 -0300

    refactor: remove unused packages and change Main stories to use controls

--- src/components/Main/Main.stories.tsx
+++ src/components/Main/Main.stories.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Meta } from '@storybook/react/types-6-0'
-import { text, withKnobs } from '@storybook/addon-knobs'
+import { Meta, Story } from '@storybook/react/types-6-0'
 
 import Main from './Main'
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
 } as Meta
 
-export const Basic = () => <Main title={text('Title', 'Won-Games')} />
+export const Basic: Story = (args) => <Main {...args} />
