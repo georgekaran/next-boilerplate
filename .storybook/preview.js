e16f55dfb426b93d5222986dc70cd68e642df171
georgekaran <georgekaran12@gmail.com>
2020-09-05 23:36:00 -0300

    feat: add withGlobalStyles decorator to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,9 @@
 
+import { addDecorator } from '@storybook/react'
+import withGlobalStyles from './withGlobalStyles'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+addDecorator(withGlobalStyles)
